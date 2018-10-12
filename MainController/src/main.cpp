0b77eeba8964ee57f8fb165fa8347420d85a8550
Arnaud Thiercelin <athiercelin@icloud.com>
2018-10-11 19:28:51 -0700

    Improved responsiveness in commands

--- MainController/src/main.cpp
+++ MainController/src/main.cpp
@@ -47,64 +47,71 @@ int main(int argc, char** argv)
   //
   
   while (true)
-  {
-    // Restrict rate
-    usleep(1000);
-
-    // Attempt to sample an event from the joystick
-    JoystickEvent event;
-    if (joystick.sample(&event))
     {
-      if (event.isButton()) {
-	if (event.number == 0) {
-	  printf("Button %u is %s\n",
-		 event.number,
-		 event.value == 0 ? "up" : "down");
-	  wiringPiI2CWrite(motorMCUfd, 100);
-	}
-      } else if (event.isAxis()) {
-	int axisNumber = event.number;
+      // Restrict rate
+      usleep(1000);
 
-	// Neg value means pushed forward on stick axis 1 and 4
-	// Positive value means pulled back.
+      // Attempt to sample an event from the joystick
+      JoystickEvent event;
+      if (joystick.sample(&event))
+	{
+	  if (event.isButton()) {
+	    if (event.number == 0) {
+	      printf("Button %u is %s\n",
+		     event.number,
+		     event.value == 0 ? "up" : "down");
+	      wiringPiI2CWrite(motorMCUfd, 100);
+	    }
+	  } else if (event.isAxis()) {
+	    int axisNumber = event.number;
+
+	    // Neg value means pushed forward on stick axis 1 and 4
+	    // Positive value means pulled back.
+	    printf("Axis %u is at position %d\n", event.number, event.value);
 	
-	if (axisNumber == 1) { // Left Stick
-	  //printf("Axis %u is at position %d\n", event.number, event.value);
-
-	 static int command = 100; // 0
-	 int newCmd = 0;
+	    if (axisNumber == 1) { // Left Stick
+	      static int command = 100; // 0
+	      int newCmd = 0;
 	 
-	  if (event.value != 0) {
-	    if (event.value > 0) {
-	      newCmd = 71;
-	    } else {
-	      newCmd = 131;
-	    }
-	  } else {
-	    newCmd = 100;
-	  }
+	      if (event.value != 0) {
+		if (event.value > 0) {
+		  newCmd = 21;
+		} else {
+		  newCmd = 181;
+		}
+	      } else {
+		newCmd = 100;
+	      }
 
-	  if (newCmd != command) {
-	    command = newCmd;
-	    printf("I2C Sending %d", command);
-	    wiringPiI2CWrite(motorMCUfd, command);
-	  }
+	      if (newCmd != command) {
+		command = newCmd;
+		printf("I2C Sending %d", command);
+		wiringPiI2CWrite(motorMCUfd, command);
+	      }
 	  
-	} else if (axisNumber == 4) { // Right Stick
-	  printf("Axis %u is at position %d\n", event.number, event.value);
+	    }
+	    if (axisNumber == 4) { // Right Stick
+	      static int command = 100; // 0
+	      int newCmd = 0;
+	 
+	      if (event.value != 0) {
+		if (event.value > 0) {
+		  newCmd = 20;
+		} else {
+		  newCmd = 180;
+		}
+	      } else {
+		newCmd = 100;
+	      }
 
-	  int command = 100; // 0
-	  if (event.value != 0) {
-	    if (event.value > 0) {
-	      command = 102;
-	    } else {
-	      command = 98;
+	      if (newCmd != command) {
+		command = newCmd;
+		printf("I2C Sending %d", command);
+		wiringPiI2CWrite(motorMCUfd, command);
+	      }
+	
 	    }
 	  }
-	  wiringPiI2CWrite(motorMCUfd, command);
 	}
-	
-      }
     }
-  }
 }
